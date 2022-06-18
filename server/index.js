63b6449a7848c0c84ba999c17075b7b6e10b1f80
Madhur Garg <madhur0308.cse19@chitkara.edu.in>
2022-06-18 15:39:08 +0530

    deployment bug fixed

--- server/index.js
+++ server/index.js
@@ -23,6 +23,10 @@ mongoose.connect(process.env.DB_URI)
 //deployment
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build'));
+    })
 }
 
 let server = app.listen(PORT,()=>{
